832411fc66dc5f2154035090e87caa6f406f5c31
nerwinalamas <nerwinalamas@gmail.com>
2024-08-12 07:10:37 +0800

    feat: add create and edit todo modal

--- app/(todo)/_layout.tsx
+++ app/(todo)/_layout.tsx
@@ -4,7 +4,15 @@ const TodoLayout = () => {
     return (
         <Stack>
             <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Stack.Screen name="[id]" options={{ headerShown: false, presentation: "modal" }} />
+            <Stack.Screen name="[id]" options={{ headerShown: false }} />
+            <Stack.Screen
+                name="createTodo"
+                options={{ presentation: "modal", headerTitle: "Create Todo" }}
+            />
+            <Stack.Screen
+                name="editTodo"
+                options={{ presentation: "modal", headerTitle: "Edit Todo" }}
+            />
         </Stack>
     );
 };
