b75fb3fd582f672e2be808fe67e8cbd7e04d6596
nerwinalamas <nerwinalamas@gmail.com>
2024-08-15 09:17:13 +0800

    feat: forgot password screen

---
+++ app/(auth)/forgotPassword.tsx
@@ -0,0 +1,53 @@
+import { router } from "expo-router";
+import { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Alert } from "react-native";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { app } from "@/config/firebaseConfig";
+
+const ForgotPassword = () => {
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = async () => {
+        try {
+            const auth = getAuth(app);
+            const response = await sendPasswordResetEmail(
+                auth,
+                email,
+            );
+
+            router.push("/");
+        } catch (error) {
+            const errorMessage = (error as Error).message;
+            Alert.alert("Error in Forgot Password: ", errorMessage);
+        }
+    };
+
+    return (
+        <View className="p-4 flex-1 justify-center items-center bg-slate-200">
+            <View className="w-full space-y-3">
+                <Text className="text-3xl font-bold text-center mb-2">
+                    Forgot Password
+                </Text>
+                <TextInput
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={setEmail}
+                    className="grow p-3 rounded-md bg-slate-100"
+                />
+                <TouchableOpacity onPress={handleSubmit} className="p-4 rounded-lg bg-yellow-400">
+                    <Text className="text-white font-semibold text-center">
+                        Send
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => router.push("/register")}>
+                    <Text className="font-medium text-center">
+                        Don't have an account?{" "}
+                        <Text className="text-blue-600">Register</Text>
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+export default ForgotPassword;
