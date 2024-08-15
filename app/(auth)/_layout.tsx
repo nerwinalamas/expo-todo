f66c405bda36610402221c9f5dad18385d9e4a8d
nerwinalamas <nerwinalamas@gmail.com>
2024-08-15 09:16:54 +0800

    feat: auth layout

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,16 @@
+import { Stack } from "expo-router";
+
+const AuthLayout = () => {
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="register" options={{ headerShown: false }} />
+            <Stack.Screen
+                name="forgotPassword"
+                options={{ headerShown: false }}
+            />
+        </Stack>
+    );
+};
+
+export default AuthLayout;
