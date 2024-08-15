2f8be13084d949d2802ff19d5f7f856f3e64ff9e
nerwinalamas <nerwinalamas@gmail.com>
2024-08-15 09:19:19 +0800

    refactor: update name

--- app/(todo)/_layout.tsx
+++ app/(todo)/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 const TodoLayout = () => {
     return (
         <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="todoList" options={{ headerShown: false }} />
             <Stack.Screen name="[id]" options={{ headerShown: false }} />
             <Stack.Screen
                 name="createTodo"
