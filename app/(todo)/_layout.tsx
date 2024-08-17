ebe98348f39f9bd4a19c2e0be722680d2454bfb6
nerwinalamas <nerwinalamas@gmail.com>
2024-08-17 14:05:13 +0800

    feat: set up tanstack query

--- app/(todo)/_layout.tsx
+++ app/(todo)/_layout.tsx
@@ -1,19 +1,33 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+
+const queryClient = new QueryClient()
 
 const TodoLayout = () => {
     return (
-        <Stack>
-            <Stack.Screen name="todoList" options={{ headerShown: false }} />
-            <Stack.Screen name="[id]" options={{ headerShown: false }} />
-            <Stack.Screen
-                name="createTodo"
-                options={{ presentation: "modal", headerTitle: "Create Todo" }}
-            />
-            <Stack.Screen
-                name="editTodo"
-                options={{ presentation: "modal", headerTitle: "Edit Todo" }}
-            />
-        </Stack>
+        <QueryClientProvider client={queryClient}>
+            <Stack>
+                <Stack.Screen
+                    name="todoList"
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen name="[id]" options={{ headerShown: false }} />
+                <Stack.Screen
+                    name="createTodo"
+                    options={{
+                        presentation: "modal",
+                        headerTitle: "Create Todo",
+                    }}
+                />
+                <Stack.Screen
+                    name="editTodo"
+                    options={{
+                        presentation: "modal",
+                        headerTitle: "Edit Todo",
+                    }}
+                />
+            </Stack>
+        </QueryClientProvider>
     );
 };
 
