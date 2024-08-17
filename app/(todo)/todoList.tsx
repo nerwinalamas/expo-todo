f0b2badd8aed6de95f8a61caa3ba31e6e5943b1a
nerwinalamas <nerwinalamas@gmail.com>
2024-08-17 14:07:31 +0800

    refactor: get all todos

--- app/(todo)/todoList.tsx
+++ app/(todo)/todoList.tsx
@@ -7,46 +7,66 @@ import {
     View,
 } from "react-native";
 import { Ellipsis, Plus } from "lucide-react-native";
-import { DATA } from "@/data";
 import { router } from "expo-router";
 import { useState } from "react";
-import { getAuth, signOut } from "firebase/auth";
-import { app } from "@/config/firebaseConfig";
+import { signOut } from "firebase/auth";
+import { auth } from "@/config/firebaseConfig";
+import { useQuery } from "@tanstack/react-query";
+import { getTodos } from "@/api";
+import { Todo } from "@/types";
 
 const TodoList = () => {
     const [isSearching, setIsSearching] = useState("");
-    const [filteredData, setFilteredData] = useState(DATA);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [triggerSearch, setTriggerSearch] = useState(false);
     const [isToggle, setIsToggle] = useState(false);
 
+    const user = auth.currentUser;
+    const userId = user ? user.uid : "";
+
+    const {
+        data: todos = [],
+        isLoading,
+        error,
+    } = useQuery<Todo[]>({
+        queryKey: ["todos", userId, searchTerm],
+        queryFn: () => getTodos(userId, triggerSearch ? searchTerm : undefined),
+        enabled: !!userId,
+    });
+
     const handleSearch = () => {
-        if (isSearching.trim() === "") {
-            setFilteredData(DATA);
-        } else {
-            const result = DATA.filter((item) =>
-                item.title.toLowerCase().includes(isSearching.toLowerCase())
-            );
-            setFilteredData(result);
-        }
+        setTriggerSearch(true);
+        setIsToggle(false);
+        setSearchTerm(isSearching);
     };
 
     const handleInputChange = (newText: string) => {
         setIsSearching(newText);
+        setIsToggle(false);
 
         if (newText.trim() === "") {
-            setFilteredData(DATA);
+            setTriggerSearch(false);
+            setSearchTerm("");
         }
     };
 
     const handleLogout = async () => {
-        const auth = getAuth(app);
         await signOut(auth);
-        router.push("/");
+        setIsToggle(false);
+        router.replace("/");
     };
 
     return (
         <SafeAreaView className="relative p-4 flex-1 flex flex-col justify-start items-center gap-2 bg-slate-200">
             <View className="w-full px-2 flex-row justify-between items-center">
-                <Text className="text-2xl font-bold">Todo</Text>
+                <TouchableOpacity
+                    onPress={() => {
+                        router.replace("/todoList");
+                        setIsToggle(false);
+                    }}
+                >
+                    <Text className="text-2xl font-bold">Todo</Text>
+                </TouchableOpacity>
                 <View className="relative">
                     <Ellipsis
                         color="black"
@@ -55,6 +75,16 @@ const TodoList = () => {
                     />
                     {isToggle && (
                         <View className="absolute top-7 right-5 w-32 space-y-6 p-4 bg-slate-900 border rounded-md shadow-lg z-10">
+                            <TouchableOpacity
+                                onPress={() => {
+                                    router.push("/(account)/profile");
+                                    setIsToggle(false);
+                                }}
+                            >
+                                <Text className="text-white text-xl">
+                                    Account
+                                </Text>
+                            </TouchableOpacity>
                             <TouchableOpacity onPress={handleLogout}>
                                 <Text className="text-white text-xl">
                                     Logout
@@ -66,7 +96,10 @@ const TodoList = () => {
             </View>
             <TouchableOpacity
                 className="absolute right-5 bottom-10 z-10 p-4 rounded-full bg-yellow-400"
-                onPress={() => router.push("/createTodo")}
+                onPress={() => {
+                    router.push("/createTodo");
+                    setIsToggle(false);
+                }}
             >
                 <Text>
                     <Plus color="white" size={25} />
@@ -90,23 +123,34 @@ const TodoList = () => {
 
             <View className="flex-1 w-full">
                 <ScrollView className="p-2 flex flex-col">
-                    {filteredData.map((d) => (
-                        <TouchableOpacity
-                            key={d.id}
-                            onPress={() => router.push(`/${d.id}`)}
-                            className="p-5 mb-3 rounded-md bg-slate-100"
-                        >
-                            <Text
-                                className={`${
-                                    d.status === "Completed"
-                                        ? "line-through"
-                                        : ""
-                                }`}
+                    {isLoading ? (
+                        <Text>Loading...</Text>
+                    ) : error ? (
+                        <Text>Error: {error.message}</Text>
+                    ) : todos.length > 0 ? (
+                        todos?.map((todo) => (
+                            <TouchableOpacity
+                                key={todo.id}
+                                onPress={() => {
+                                    router.push(`/${todo.id}`);
+                                    setIsToggle(false);
+                                }}
+                                className="p-5 mb-3 rounded-md bg-slate-100"
                             >
-                                {d.title}
-                            </Text>
-                        </TouchableOpacity>
-                    ))}
+                                <Text
+                                    className={`${
+                                        todo.status === "Completed"
+                                            ? "line-through"
+                                            : ""
+                                    }`}
+                                >
+                                    {todo.title}
+                                </Text>
+                            </TouchableOpacity>
+                        ))
+                    ) : !todos.length ? (
+                        <Text>No todos found</Text>
+                    ) : null}
                 </ScrollView>
             </View>
         </SafeAreaView>
