f477365b55fd729be3899d5be08d6d37268b57fd
nerwinalamas <nerwinalamas@gmail.com>
2024-08-18 13:52:47 +0800

    feat: edit todo

--- app/(todo)/editTodo.tsx
+++ app/(todo)/editTodo.tsx
@@ -1,6 +1,10 @@
-import { router } from "expo-router";
-import { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { getTodoById, updateTodoById } from "@/api";
+import { Todo } from "@/types";
+import { useQuery } from "@tanstack/react-query";
+import { router, useLocalSearchParams } from "expo-router";
+import { Timestamp } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Alert } from "react-native";
 import { RadioButton } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -10,23 +14,65 @@ const EditTodo = () => {
     const [editTitle, setEditTitle] = useState("");
     const [editDescription, setEditDescription] = useState("");
     const [editStatus, setEditStatus] = useState("");
+    const { id: todoId } = useLocalSearchParams<{ id: string }>();
+
+    const {
+        data: todoItem,
+        isLoading,
+        error,
+    } = useQuery<Todo | null>({
+        queryKey: ["todos", todoId],
+        queryFn: () => getTodoById(todoId),
+        enabled: !!todoId,
+    });
+
+    useEffect(() => {
+        if (todoItem) {
+            setEditTitle(todoItem.title);
+            setEditDescription(todoItem.description);
+            setEditStatus(todoItem.status);
+        }
+    }, [todoItem]);
 
     const handleStatus = (status: string) => {
         setEditStatus(status);
     };
 
-    const handleSubmit = () => {
+    const handleSubmit = async () => {
         if (!editTitle || !editDescription || !editStatus) {
-            alert("Please fill out all fields.");
+            Alert.alert("Please fill out all fields.");
             return;
         }
 
-        console.log("Title:", editTitle);
-        console.log("Description:", editDescription);
-        console.log("Status:", editStatus);
+        try {
+            const updatedTodo = {
+                title: editTitle,
+                description: editDescription,
+                status: editStatus,
+                updatedAt: Timestamp.now(),
+            };
 
-        router.replace("/todoList");
+            updateTodoById(todoId, updatedTodo);
+            router.replace("/todoList");
+        } catch (error) {
+            console.error("Error updating todo: ", error);
+            Alert.alert("Error updating todo");
+        }
     };
+
+    if (isLoading)
+        return (
+            <SafeAreaView className="flex-1 justify-start items-center gap-3 p-4 bg-slate-200">
+                <Text>Loading...</Text>
+            </SafeAreaView>
+        );
+
+    if (error)
+        return (
+            <SafeAreaView className="flex-1 justify-start items-center gap-3 p-4 bg-slate-200">
+                <Text>Error: {error.message}</Text>
+            </SafeAreaView>
+        );
 
     return (
         <SafeAreaView className="flex-1 justify-start items-center gap-3 p-4 bg-slate-200">
