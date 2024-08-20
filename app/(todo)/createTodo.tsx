f01c41c140621f84a09450cc63233dbd2704b804
nerwinalamas <nerwinalamas@gmail.com>
2024-08-20 10:22:28 +0800

    feat: add validation and loading state

--- app/(todo)/createTodo.tsx
+++ app/(todo)/createTodo.tsx
@@ -4,14 +4,41 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { router } from "expo-router";
 import { addDoc, Timestamp } from "firebase/firestore";
 import { auth, todosCollection } from "@/config/firebaseConfig";
+import { InputError, TodoTitle } from "@/types";
+import { todoSchema } from "@/utils/schema";
 
 const CreateTodo = () => {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
+    const [errors, setErrors] = useState<TodoTitle>({});
+    const [loading, setLoading] = useState(false);
+
+    const handleTitleChange = (value: string) => {
+        setTitle(value);
+
+        if (errors.title) {
+            setErrors((prev) => ({ ...prev, title: "" }));
+        }
+    };
 
     const handleSubmit = async () => {
         const user = auth.currentUser;
         const userId = user ? user.uid : null;
+
+        const validation = todoSchema.safeParse({ title, description });
+
+        if (!validation.success) {
+            const newErrors: InputError = {};
+            validation.error.errors.forEach((error) => {
+                if (error.path.includes("title")) {
+                    newErrors.title = error.message;
+                }
+            });
+            setErrors(newErrors);
+            return;
+        }
+
+        setLoading(true);
 
         try {
             const newTodo = {
@@ -24,14 +51,17 @@ const CreateTodo = () => {
             };
 
             await addDoc(todosCollection, newTodo);
+            setLoading(false);
             router.replace("/todoList");
         } catch (error) {
             console.error("Error creating todo: ", error);
             Alert.alert("Error creating todo");
+        }  finally {
+            setLoading(false);
+            setTitle("");
+            setDescription("");
+            setErrors({});
         }
-
-        setTitle("");
-        setDescription("");
     };
 
     return (
@@ -41,9 +71,12 @@ const CreateTodo = () => {
                 <TextInput
                     placeholder="Enter Title"
                     value={title}
-                    onChangeText={(newText) => setTitle(newText)}
+                    onChangeText={handleTitleChange}
                     className="grow p-3 rounded-md bg-slate-100"
                 />
+                {errors.title && (
+                    <Text className="text-red-500">{errors.title}</Text>
+                )}
             </View>
             <View className="w-full space-y-1">
                 <Text>Description:</Text>
@@ -59,13 +92,17 @@ const CreateTodo = () => {
                 <TouchableOpacity
                     onPress={handleSubmit}
                     className="p-4 rounded-lg bg-yellow-400"
+                    disabled={loading}
                 >
-                    <Text className="text-white text-center">Create</Text>
+                    <Text className="text-white text-center">
+                        {loading ? "Loading..." : "Create"}
+                    </Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity
                     onPress={() => router.replace("/todoList")}
                     className="p-4 rounded-lg bg-slate-300"
+                    disabled={loading}
                 >
                     <Text className="text-center">Cancel</Text>
                 </TouchableOpacity>
